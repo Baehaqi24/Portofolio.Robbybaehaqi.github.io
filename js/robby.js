ecbe316c9fbe81760e4f2650fc571c5617b31d53
robbybaehaqi <robby.baehaqi@raharja.info>
2022-06-13 13:01:23 +0700

    Merubah bagian contact & menambahkan text event bagian header

--- js/robby.js
+++ js/robby.js
@@ -10,7 +10,32 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// Gmail
+// Event Ngetik
+
+const hero = ["Robby baehaqi"];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = "";
+let words = "";
+
+(function ngetik() {
+  if (count == hero.length) {
+    count = 0;
+  }
+
+  currentTxt = hero[count];
+
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector(".efek-ngetik").textContent = words;
+
+  if (words.length == currentTxt.length) {
+    count++;
+    txtIndex = 0;
+  }
+  setTimeout(ngetik, 300);
+})();
+
+// Integrasi dengan gmail
 
 const form = document.querySelector('.contact-form');
 form.addEventListener('submit',(e)=>{
@@ -30,10 +55,10 @@ function sendEmail(username, subject, email, message){
         Host : "smtp.elasticemail.com",
         Username : "robbybaehaqi8@gmail.com",
         Password : "EE66FD6C93B358062BEE8AF1C0E50BA63ED4",
-        To : 'robbybaehaqi8@gmail.com',
+        To : "robbybaehaqi8@gmail.com",
         From : "robbybaehaqi8@gmail.com",
-        Subject : `${subject} just message you from the website form`,
-        Body : `name: ${username} <br> subject: ${subject} <br> email: ${email} <br> message ${message} <br>`,
+        Subject : `${username} Memberikan Pesan Kepada Anda, Yukk Check Sekarang`,
+        Body : `name: ${username} <br> subject: ${subject} <br> email: ${email} <br> message: ${message} <br>`,
     }).then((success)=>{
         alert('Pesan Anda Sudah terikirim');
     }).catch((error)=>{
